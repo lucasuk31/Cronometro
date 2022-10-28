9bedfc7693d702795481cd6a57f2e5dd9492ef11
Lucas Santos <100493171+lucasuk31@users.noreply.github.com>
2022-10-28 17:02:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let sec = 0;
+let min = 0;
+let hour = 0;
+let interval;
+
+function zero(digit) {
+    if (digit < 10) {
+        return ('0' + digit);
+    } else {
+        return (digit);
+    }
+}
+
+function start() {
+    interval = setInterval(watch, 1000);
+}
+
+function pause() {
+    clearInterval(interval);
+}
+
+function stop() {
+    clearInterval(interval);
+    sec = 0;
+    min = 0;
+    hour = 0;
+    document.querySelector('#timer').innerText = '00:00:00';
+}
+
+function watch() {
+    sec++;
+    if (sec == 60) {
+        min++;
+        sec = 0;
+        if (min == 60) {
+            min = 0;
+            hour++;
+        }
+    } 
+
+    document.querySelector('#timer').innerText = zero(hour) + ':' + zero(min) + ':' + zero(sec);
+}
